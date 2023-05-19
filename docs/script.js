e4822528913f207e7be05e51499657286cf634bc
Danny W <dannywang@utexas.edu>
2023-05-18 23:42:56 -0400

    add check password length function

--- docs/script.js
+++ docs/script.js
@@ -1,18 +1,33 @@
 // Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
+const generateBtn = document.querySelector("#generate");
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  const password = generatePassword();
+  const passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 }
 
 // Assignment code here
 function generatePassword() {
-  return "Hello World";
+  const passwordOptions = createPasswordOptions();
+
+  return passwordOptions;
+}
+
+function createPasswordOptions() {
+  const passwordLength = prompt(
+    "How long would you like the length of your password to be? (Must be 10 to 128 chars in length)"
+  );
+
+  if (passwordLength < 10 || passwordLength > 128) {
+    alert("Please try again; password length must be 10-128 chars in length");
+    return null;
+  }
+
+  return passwordLength;
 }
